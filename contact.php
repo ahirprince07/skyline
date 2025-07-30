8b064912f48b934bccf880dadba6d42c5a5f5be2
ahirprince07 <princebheda8188@gmail.com>
2025-07-30 21:27:10 +0530

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>THE SKYLINE - CONTACT</title>
+    <?php require('inc/links.php'); ?>
+</head>
+<body class="bg-light">
+
+    <?php require('inc/header.php'); ?>
+
+    <div class="my-5 px-4">
+        <h2 class="fw-bold h-font text-center">CONTACT US</h2>
+        <div class="h-line bg-dark"></div>
+        <p class="text-center mt-3">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Amet mollitia minus quaerat blanditiis <br> dolorum expedita neque repellat, 
+            ipsam eveniet! Inventore.
+        </p>
+    </div>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6 col-md-6 mb-5 px-4">
+                <div class="bg-white rounded shadow p-4">
+                    <iframe class="w-100 rounded mb-4" height="320px" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d29677.22148494419!2d71.22087908862304!3d21.599476153666803!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x395880c72d516845%3A0x4e950cffb505bb12!2sAmreli%2C%20Gujarat%20365601!5e0!3m2!1sen!2sin!4v1753199157642!5m2!1sen!2sin" loading="lazy"></iframe>
+                    <h5>Address</h5>
+                    <a href="https://maps.app.goo.gl/gTuFf4aPPSYWXs556" target="_blank" class="d-lnline-block text-decoration-none text-dark mb-2">
+                        <i class="bi bi-geo-alt-fill"></i>thebi dam, chital road, amreli-365601
+                    </a>
+                    <h5 class="mt-4">Call Us</h5>
+                    <a href="tel: +919099084997" class="d-inline-block mb-2 text-decoration-none text-dark">
+                       <i class="bi bi-telephone-fill"></i> +919099084997
+                    </a>
+                    <br>
+                    <a href="tel: +919099084997" class="d-inline-block text-decoration-none text-dark">
+                       <i class="bi bi-telephone-fill"></i> +919586084997
+                    </a>
+                    <h5 class="mt-4">Email</h5>
+                    <a href="mailto: princebheda8188@gmail.com" class="d-inline-block text-decoration-none text-dark">
+                       <i class="bi bi-envelope-fill"></i> princebheda8188@gamil.com
+                    </a>
+                    <h5 class="mt-4">Follow Us</h5>
+                    <a href="#" class="d-inline-block text-dark fs-5 me-2">
+                        <i class="bi bi-twitter-x me-1"></i>
+                    </a>
+                    <a href="#" class="d-inline-block text-dark fs-5 me-2">
+                        <i class="bi bi-facebook me-1"></i>
+                    </a>
+                    <a href="#" class="d-inline-block text-dark fs-5 ">
+                        <i class="bi bi-instagram me-1"></i>
+                    </a>
+                </div>
+            </div>
+            <div class="col-lg-6 col-md-6 px-4">
+                <div class="bg-white rounded shadow p-4">
+                    <form>
+                        <h5>Send a message</h5>
+                        <div class="mt-3">
+                            <label class="form-label" style="font-weight: 500;">Name</label>
+                            <input type="text" class="form-control shadoe-none">
+                        </div>
+                        <div class="mt-3">
+                            <label class="form-label" style="font-weight: 500;">Email</label>
+                            <input type="email" class="form-control shadoe-none">
+                        </div>
+                        <div class="mt-3">
+                            <label class="form-label" style="font-weight: 500;">Subject</label>
+                            <input type="text" class="form-control shadoe-none">
+                        </div>
+                        <div class="mt-3">
+                            <label class="form-label" style="font-weight: 500;">Message</label>
+                            <textarea class="form-control shadow-none"  rows="5" style="resize: none;"></textarea>
+                        </div>
+                        <button type="submit" class="btn text-white custom-bg mt-3">SEND</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <?php require('inc/footer.php'); ?>
+
+</body>
+</html>
